10a3727341e1389ddc7058ad7fc39b38ecf4700e
jday100 <jdayterminator@outlook.com>
2023-12-31 20:14:10 +0800

    modify

--- Projects/T200/Source/T200TEST/T200Home.js
+++ Projects/T200/Source/T200TEST/T200Home.js
@@ -2,4 +2,8 @@ const T200Test = require('./lib/T200Test.js');
 
 let test = new T200Test();
 
-test.run();
+//test.run();
+
+let name = Object.getOwnPropertyNames(Object.getPrototypeOf(test));
+
+console.log(name);
---
+++ Projects/T200/Source/T200TEST/case/T200Register.js
@@ -0,0 +1,11 @@
+class T200Register {
+    constructor() {
+
+    }
+
+    test() {
+
+    }
+}
+
+module.exports = T200Register;
