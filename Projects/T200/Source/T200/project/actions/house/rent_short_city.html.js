1f7e028aa68212dc1ab09bda1d933cd61282a7bb
jday100 <jdayterminator@outlook.com>
2024-01-28 14:58:46 +0800

    modify

--- Projects/T200/Source/T200/project/actions/house/rent_short_city.html.js
+++ Projects/T200/Source/T200/project/actions/house/rent_short_city.html.js
@@ -49,4 +49,52 @@ async function do_house_rent_short_city_list(request, response, cookie, session,
 }
 
 
+async function do_house_rent_short_city_search(request, response, cookie, session, resource) {
+    log(__filename, "do_house_rent_short_city_search");
+    let self = this;
+    let promise = new Promise(function(resolve, reject){
+        let house = new T200UserHouseRent();
+        let UserBiz = new T200HomeUserBiz(request, cookie, session);
+
+        let city_id = request.get("id");
+        let search = request.get("search");
+
+        if(T200HttpsForm.verify_id(city_id)
+            && T200HttpsForm.verify_text(search)){
+            house.city_id = city_id;
+            house._search = search;
+            house.flash_paging_fields();
+            house.flash_fulltext_fields();
+            house.merge_fulltext_count = house.merge_user_fulltext_short_city_count;
+            house.merge_fulltext_list = house.merge_user_fulltext_short_city_list;
+            UserBiz.fulltext2(house).then(function(result){
+                let view = new T200HomeView(resource);
+                let data = {};
+                data.city_id = city_id;
+                data.paging = result.paging;
+                data.houses = result.values;
+                data.search = house._search;
+                return view.render_file("house/rent_short_city.ejs", data).then(function (value) {
+                    response.type("json");
+                    resolve(value);
+                }, function (err) {
+                    response.type("json");
+                    reject();
+                });
+            }, function (err) {
+                response.type("json");
+                reject();
+            });
+        }else{
+            response.type("json");
+            reject();
+        }
+  
+    });
+
+    return promise;
+}
+
+
 global.action.use_post('/house/rent/short/city/list', do_house_rent_short_city_list);
+global.action.use_post('/house/rent/short/city/search', do_house_rent_short_city_search);
