30f537d72f9e4f3d170f2c7f6de9b5e1561dd095
jday100 <jdayterminator@outlook.com>
2024-02-11 23:47:51 +0800

    modify

--- Projects/T100/Native/T100/Source/T100OS/system/boot/boot.asm
+++ Projects/T100/Native/T100/Source/T100OS/system/boot/boot.asm
@@ -1,25 +1,62 @@
-//init
-
-//print
+org     0x7c00
 
 
-//load
+main:
+    mov     ax,     cs
+    mov     ss,     ax
+    mov     ds,     ax
+    mov     es,     ax 
+
+    mov     ax,     msg
+    call    print 
+
+    mov     bx,     0x8200
+    mov     ah,     0x02
+    mov     al,     0x01
+    mov     cx,     0x01
+    mov     dx,     0x00
+    call    load
+    jc      stop
+
+    mov     ax,     msg_end
+    call    print 
+
+    jmp     stop 
 
 
-//search
 
 
-//load
+print:
+    mov     si,     ax
+print_next:
+    mov     al,     [si]
+    add     si,     1
+    cmp     al,     0x00
+    je      print_end
 
-//hlt
+    mov     ah,     0x0e
+    mov     bx,     0x0f
+    int     0x10
+    jmp     print_next
 
-//function
-//print
-
-
-//load
-
-//search
+print_end:
+    ret 
 
 
-//data
+load:
+    int     0x13
+    ret
+
+
+stop:
+    nop
+    jmp     stop
+
+
+msg:
+    db  "hello world!", 0x0a, 0x00
+msg_end:
+    db  "stop", 0x0a
+    times   510-($-$$)  db 0x00
+
+    db  0x55, 0xaa
