6339fb165ee1bc829160067299aa7820f4283052
jday100 <jdayterminator@outlook.com>
2025-01-04 18:59:03 +0800

    Modify

--- Projects/Game/Source/T100DX12/include/game/T100Game.h
+++ Projects/Game/Source/T100DX12/include/game/T100Game.h
@@ -4,6 +4,12 @@
 #include <string>
 #include <windows.h>
 #include "d3d12.h"
+#include <dxgi1_6.h>
+#include <D3Dcompiler.h>
+#include <DirectXMath.h>
+#include "d3dx12.h"
+#include <wrl.h>
+#include <shellapi.h>
 #include "T100Common.h"
 
 class T100Game
