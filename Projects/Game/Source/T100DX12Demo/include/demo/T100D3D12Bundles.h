657afc0f0f184fd00bdc7424432a991894225b6a
jday100 <jdayterminator@outlook.com>
2025-01-06 18:26:31 +0800

    Modify

--- Projects/Game/Source/T100DX12Demo/include/demo/T100D3D12Bundles.h
+++ Projects/Game/Source/T100DX12Demo/include/demo/T100D3D12Bundles.h
@@ -1,16 +1,78 @@
 #ifndef T100D3D12BUNDLES_H
 #define T100D3D12BUNDLES_H
 
+#include "T100DXSample.h"
+#include "T100StepTimer.h"
+#include "T100FrameResource.h"
+#include "T100SimpleCamera.h"
 
-class T100D3D12Bundles
+using namespace DirectX;
+using Microsoft::WRL::ComPtr;
+
+class T100D3D12Bundles : public T100DXSample
 {
     public:
-        T100D3D12Bundles();
+        T100D3D12Bundles(UINT width, UINT height, std::wstirng name);
         virtual ~T100D3D12Bundles();
+
+        virtual void            OnInit();
+        virtual void            OnUpdate();
+        virtual void            OnRender();
+        virtual void            OnDestroy();
+
+        virtual void            OnKeyDown(UINT8 key);
+        virtual void            OnKeyUp(UINT8 key);
 
     protected:
 
     private:
+        static const UINT       FrameCount              = 3;
+        static const UINT       CityRowCount            = 10;
+        static const UINT       CityColumnCount         = 3;
+        static const bool       UseBundles              = true;
+
+        CD3DX12_VIEWPORT                    m_viewport;
+        CD3DX12_RECT                        m_scissorRect;
+        ComPtr<IDXGISwapChain3>             m_swapChain;
+        ComPtr<ID3D12Device>                m_device;
+        ComPtr<ID3D12Resource>              m_renderTargets[FrameCount];
+        ComPtr<ID3D12Resource>              m_depthStencil;
+        ComPtr<ID3D12CommandAllocator>      m_commandAllocator;
+        ComPtr<ID3D12CommandQueue>          m_commandQueue;
+        ComPtr<ID3D12RootSignature>         m_rootSignature;
+        ComPtr<ID3D12DescriptorHeap>        m_rtvHeap;
+        ComPtr<ID3D12DescriptorHeap>        m_cbvSrvHeap;
+        ComPtr<ID3D12DescriptorHeap>        m_dsvHeap;
+        ComPtr<ID3D12DescriptorHeap>        m_samplerHeap;
+        ComPtr<ID3D12PipelineState>         m_pipelineState1;
+        ComPtr<ID3D12PipelineState>         m_pipelineState2;
+        ComPtr<ID3D12GraphicsCommandList>   m_commandList;
+
+        UINT                                m_numIndices;
+        ComPtr<ID3D12Resource>              m_vertexBuffer;
+        ComPtr<ID3D12Resource>              m_indexBuffer;
+        ComPtr<ID3D12Resource>              m_texture;
+        D3D12_VERTEX_BUFFER_VIEW            m_vertexBufferView;
+        D3D12_INDEX_BUFFER_VIEW             m_indexBufferView;
+        StepTimer                           m_timer;
+        UINT                                m_cbvSrvDescriptorSize;
+        UINT                                m_rtvDescriptorSize;
+        SimpleCamera                        m_camera;
+
+        std::vector<FrameResource*>         m_frameResources;
+        FrameResource*                      m_pCurrentFrameResource;
+        UINT                                m_currentFrameResourceIndex;
+
+        UINT                                m_frameIndex;
+        UINT                                m_frameCounter;
+        HANDLE                              m_fenceEvent;
+        ComPtr<ID3D12Fence>                 m_fence;
+        UINT64                              m_fenceValue;
+
+        void                                LoadPipeline();
+        void                                LoadAssets();
+        void                                CreateFrameResources();
+        void                                PopulateCommandList(FrameResource* pFrameResource);
 };
 
 #endif // T100D3D12BUNDLES_H
--- Projects/Game/Source/T100DX12Demo/src/demo/T100FrameResource.cpp
+++ Projects/Game/Source/T100DX12Demo/src/demo/T100FrameResource.cpp
@@ -1,11 +1,115 @@
 #include "T100FrameResource.h"
 
-T100FrameResource::T100FrameResource()
+#include "stdafx.h"
+
+T100FrameResource::T100FrameResource(ID3D12Device* pDevice, UINT cityRowCount, UINT cityColumnCount) :
+    m_fenceValue(0),
+    m_cityRowCount(cityRowCount),
+    m_cityColumnCount(cityColumnCount)
 {
     //ctor
+    m_modelMatrices.resize(m_cityRowCount * m_cityColumnCount);
+
+    ThrowIfFailed(pDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator)));
+    ThrowIfFailed(pDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_BUNDLE, IID_PPV_ARGS(&m_bundleAllocator)));
+
+    ThrowIfFailed(pDevice->CreateCommittedResource(
+        &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD),
+        D3D12_HEAP_FLAG_NONE,
+        &CD3DX12_RESOURCE_DESC::Buffer(sizeof(SceneConstantBuffer) * m_cityRowCount * m_cityColumnCount),
+        D3D12_RESOURCE_STATE_GENERIC_READ,
+        nullptr,
+        IID_PPV_ARGS(&m_cbvUploadHeap)));
+
+    CD3DX12_RANGE readRange(0, 0);        // We do not intend to read from this resource on the CPU.
+    ThrowIfFailed(m_cbvUploadHeap->Map(0, &readRange, reinterpret_cast<void**>(&m_pConstantBuffers)));
+
+    SetCityPositions(8.0f, -8.0f);
 }
 
 T100FrameResource::~T100FrameResource()
 {
     //dtor
+    m_cbvUploadHeap->Unmap(0, nullptr);
+    m_pConstantBuffers = nullptr;
+}
+
+void T100FrameResource::InitBundle(ID3D12Device* pDevice, ID3D12PipelineState* pPso1, ID3D12PipelineState* pPso2,
+    UINT frameResourceIndex, UINT numIndices, D3D12_INDEX_BUFFER_VIEW* pIndexBufferViewDesc, D3D12_VERTEX_BUFFER_VIEW* pVertexBufferViewDesc,
+    ID3D12DescriptorHeap* pCbvSrvDescriptorHeap, UINT cbvSrvDescriptorSize, ID3D12DescriptorHeap* pSamplerDescriptorHeap, ID3D12RootSignature* pRootSignature)
+{
+    ThrowIfFailed(pDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_BUNDLE, m_bundleAllocator.Get(), pPso1, IID_PPV_ARGS(&m_bundle)));
+    NAME_D3D12_OBJECT(m_bundle);
+
+    PopulateCommandList(m_bundle.Get(), pPso1, pPso2, frameResourceIndex, numIndices, pIndexBufferViewDesc,
+        pVertexBufferViewDesc, pCbvSrvDescriptorHeap, cbvSrvDescriptorSize, pSamplerDescriptorHeap, pRootSignature);
+
+    ThrowIfFailed(m_bundle->Close());
+}
+
+void T100FrameResource::SetCityPositions(FLOAT intervalX, FLOAT intervalZ)
+{
+    for (UINT i = 0; i < m_cityRowCount; i++)
+    {
+        FLOAT cityOffsetZ = i * intervalZ;
+        for (UINT j = 0; j < m_cityColumnCount; j++)
+        {
+            FLOAT cityOffsetX = j * intervalX;
+
+            XMStoreFloat4x4(&m_modelMatrices[i * m_cityColumnCount + j], XMMatrixTranslation(cityOffsetX, 0.02f * (i * m_cityColumnCount + j), cityOffsetZ));
+        }
+    }
+}
+
+void T100FrameResource::PopulateCommandList(ID3D12GraphicsCommandList* pCommandList, ID3D12PipelineState* pPso1, ID3D12PipelineState* pPso2,
+    UINT frameResourceIndex, UINT numIndices, D3D12_INDEX_BUFFER_VIEW* pIndexBufferViewDesc, D3D12_VERTEX_BUFFER_VIEW* pVertexBufferViewDesc,
+    ID3D12DescriptorHeap* pCbvSrvDescriptorHeap, UINT cbvSrvDescriptorSize, ID3D12DescriptorHeap* pSamplerDescriptorHeap, ID3D12RootSignature* pRootSignature)
+{
+    pCommandList->SetGraphicsRootSignature(pRootSignature);
+
+    ID3D12DescriptorHeap* ppHeaps[] = { pCbvSrvDescriptorHeap, pSamplerDescriptorHeap };
+    pCommandList->SetDescriptorHeaps(_countof(ppHeaps), ppHeaps);
+    pCommandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    pCommandList->IASetIndexBuffer(pIndexBufferViewDesc);
+    pCommandList->IASetVertexBuffers(0, 1, pVertexBufferViewDesc);
+    pCommandList->SetGraphicsRootDescriptorTable(0, pCbvSrvDescriptorHeap->GetGPUDescriptorHandleForHeapStart());
+    pCommandList->SetGraphicsRootDescriptorTable(1, pSamplerDescriptorHeap->GetGPUDescriptorHandleForHeapStart());
+
+    UINT frameResourceDescriptorOffset = 1 + (frameResourceIndex * m_cityRowCount * m_cityColumnCount);
+    CD3DX12_GPU_DESCRIPTOR_HANDLE cbvSrvHandle(pCbvSrvDescriptorHeap->GetGPUDescriptorHandleForHeapStart(), frameResourceDescriptorOffset, cbvSrvDescriptorSize);
+
+    PIXBeginEvent(pCommandList, 0, L"Draw cities");
+    BOOL usePso1 = TRUE;
+    for (UINT i = 0; i < m_cityRowCount; i++)
+    {
+        for (UINT j = 0; j < m_cityColumnCount; j++)
+        {
+            pCommandList->SetPipelineState(usePso1 ? pPso1 : pPso2);
+            usePso1 = !usePso1;
+
+            pCommandList->SetGraphicsRootDescriptorTable(2, cbvSrvHandle);
+            cbvSrvHandle.Offset(cbvSrvDescriptorSize);
+
+            pCommandList->DrawIndexedInstanced(numIndices, 1, 0, 0, 0);
+        }
+    }
+    PIXEndEvent(pCommandList);
+}
+
+void XM_CALLCONV T100FrameResource::UpdateConstantBuffers(FXMMATRIX view, CXMMATRIX projection)
+{
+    XMMATRIX model;
+    XMFLOAT4X4 mvp;
+
+    for (UINT i = 0; i < m_cityRowCount; i++)
+    {
+        for (UINT j = 0; j < m_cityColumnCount; j++)
+        {
+            model = XMLoadFloat4x4(&m_modelMatrices[i * m_cityColumnCount + j]);
+
+            XMStoreFloat4x4(&mvp, XMMatrixTranspose(model * view * projection));
+
+            memcpy(&m_pConstantBuffers[i * m_cityColumnCount + j], &mvp, sizeof(mvp));
+        }
+    }
 }
