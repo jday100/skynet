3bb37040dfb170dba0912ecd18e54423d1c2ca38
jday100 <jdayterminator@outlook.com>
2025-01-10 19:42:27 +0800

    Modify

---
+++ Projects/Game/Source/T1003DEditor/include/dx12/T100DX12Renderer.h
@@ -0,0 +1,17 @@
+#ifndef T100DX12RENDERER_H
+#define T100DX12RENDERER_H
+
+#include "T100DX12Base.h"
+
+class T100DX12Renderer : public T100DX12Base
+{
+    public:
+        T100DX12Renderer();
+        virtual ~T100DX12Renderer();
+
+    protected:
+
+    private:
+};
+
+#endif // T100DX12RENDERER_H
---
+++ Projects/Game/Source/T1003DEditor/src/dx12/T100DX12Renderer.cpp
@@ -0,0 +1,12 @@
+#include "T100DX12Renderer.h"
+
+T100DX12Renderer::T100DX12Renderer()
+    :T100DX12Base()
+{
+    //ctor
+}
+
+T100DX12Renderer::~T100DX12Renderer()
+{
+    //dtor
+}
