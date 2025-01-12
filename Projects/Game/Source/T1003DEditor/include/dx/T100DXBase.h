bfcd67dc4547653b0328b4bf136e860a1b604e48
jday100 <jdayterminator@outlook.com>
2025-01-12 21:27:08 +0800

    Modify

--- Projects/Game/Source/T1003DEditor/include/dx/T100DXBase.h
+++ Projects/Game/Source/T1003DEditor/include/dx/T100DXBase.h
@@ -1,6 +1,16 @@
 #ifndef T100DXBASE_H
 #define T100DXBASE_H
 
+#include <windows.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <wrl.h>
+#include "d3d12/d3dx12.h"
+
+#include "T100Common.h"
+
+using Microsoft::WRL::ComPtr;
 
 class T100DXBase
 {
@@ -8,9 +18,62 @@ class T100DXBase
         T100DXBase();
         virtual ~T100DXBase();
 
+        virtual T100VOID                        Create(HWND, UINT, UINT);
+        virtual T100VOID                        Destroy();
+
+        virtual T100VOID                        Start();
+        virtual T100VOID                        Stop();
+
+        virtual T100VOID                        SetSize(UINT, UINT);
+
+        virtual T100VOID                        Render();
+
+        UINT                                    m_cityRowCount;
+        UINT                                    m_cityColumnCount;
+
+
     protected:
+        HWND                                    m_hwnd;
+        UINT                                    m_width;
+        UINT                                    m_height;
+
+        static const UINT                       m_frameCount            = 3;
+
+        T100BOOL                                m_useWarpDevice;
+
+        UINT                                    m_frameIndex;
+
+        UINT                                    m_rtvDescriptorSize;
+        UINT                                    m_cbvSrvDescriptorSize;
+
+        ComPtr<IDXGIFactory4>                   m_factory;
+        ComPtr<ID3D12Device>                    m_device;
+        ComPtr<IDXGISwapChain3>                 m_swapChain;
+        ComPtr<ID3D12CommandQueue>              m_commandQueue;
+        ComPtr<ID3D12CommandAllocator>          m_commandAllocator;
+
+        ComPtr<ID3D12DescriptorHeap>            m_rtvHeap;
+        ComPtr<ID3D12DescriptorHeap>            m_dsvHeap;
+        ComPtr<ID3D12DescriptorHeap>            m_cbvSrvHeap;
+        ComPtr<ID3D12DescriptorHeap>            m_samplerHeap;
 
     private:
+        T100VOID                                LoadPipeline();
+
+        T100VOID                                GetHardwareAdapter(
+                                                                   _In_ IDXGIFactory1* pFactory,
+                                                                   _Outptr_result_maybenull_ IDXGIAdapter1** ppAdapter,
+                                                                   T100BOOL requestHighPerformanceAdapter = T100FALSE
+                                                                   );
+        T100VOID                                CreateFactory();
+        T100VOID                                CreateDevice();
+        T100VOID                                CreateCommandQueue();
+        T100VOID                                CreateSwapChain();
+        T100VOID                                CreateRtvHeap();
+        T100VOID                                CreateDsvHeap();
+        T100VOID                                CreateCbvHeap();
+        T100VOID                                CreateSamplerHeap();
+        T100VOID                                CreateCommandAllocator();
 };
 
 #endif // T100DXBASE_H
--- Projects/Game/Source/T1003DEditor/src/dx/T100DXBase.cpp
+++ Projects/Game/Source/T1003DEditor/src/dx/T100DXBase.cpp
@@ -1,5 +1,7 @@
 #include "T100DXBase.h"
 
+#include "T100DX12Tools.h"
+
 T100DXBase::T100DXBase()
 {
     //ctor
@@ -8,4 +10,274 @@ T100DXBase::T100DXBase()
 T100DXBase::~T100DXBase()
 {
     //dtor
+}
+
+T100VOID T100DXBase::Create(HWND hwnd, UINT width, UINT height)
+{
+    m_hwnd      = hwnd;
+    SetSize(width, height);
+}
+
+T100VOID T100DXBase::Destroy()
+{
+
+}
+
+T100VOID T100DXBase::Start()
+{
+
+}
+
+T100VOID T100DXBase::Stop()
+{
+
+}
+
+T100VOID T100DXBase::SetSize(UINT width, UINT height)
+{
+
+}
+
+T100VOID T100DXBase::Render()
+{
+
+}
+
+T100VOID T100DXBase::LoadPipeline()
+{
+    CreateFactory();
+    CreateDevice();
+    CreateCommandQueue();
+    CreateSwapChain();
+    CreateRtvHeap();
+    CreateDsvHeap();
+    CreateCbvHeap();
+    CreateSamplerHeap();
+    CreateCommandAllocator();
+}
+
+T100VOID T100DXBase::CreateFactory()
+{
+    UINT        dxgiFactoryFlags        = 0;
+
+    ThrowIfFailed(CreateDXGIFactory2(
+                                     dxgiFactoryFlags,
+                                     IID_PPV_ARGS(&m_factory)
+                                     ));
+}
+
+T100VOID T100DXBase::CreateDevice()
+{
+    if(m_useWarpDevice)
+    {
+        ComPtr<IDXGIAdapter>        warpAdapter;
+
+        ThrowIfFailed(m_factory->EnumWarpAdapter(IID_PPV_ARGS(&warpAdapter)));
+
+        ThrowIfFailed(D3D12CreateDevice(
+                                        warpAdapter.Get(),
+                                        D3D_FEATURE_LEVEL_12_0,
+                                        IID_PPV_ARGS(&m_device)
+                                        ));
+    }
+    else
+    {
+        ComPtr<IDXGIAdapter1>           hardwareAdapter;
+        GetHardwareAdapter(m_factory.Get(), &hardwareAdapter);
+
+        ThrowIfFailed(D3D12CreateDevice(
+                                        hardwareAdapter.Get(),
+                                        D3D_FEATURE_LEVEL_12_0,
+                                        IID_PPV_ARGS(&m_device)
+                                        ));
+    }
+}
+
+_Use_decl_annotations_
+T100VOID T100DXBase::GetHardwareAdapter(
+                                          IDXGIFactory1* pFactory,
+                                          IDXGIAdapter1** ppAdapter,
+                                          T100BOOL requestHighPerformanceAdapter
+                                          )
+{
+    *ppAdapter          = T100NULL;
+
+    ComPtr<IDXGIAdapter1>       adapter;
+    ComPtr<IDXGIFactory6>       factory6;
+
+    if(SUCCEEDED(pFactory->QueryInterface(IID_PPV_ARGS(&factory6))))
+    {
+        for(
+            UINT adapterIndex = 0;
+            SUCCEEDED(factory6->EnumAdapterByGpuPreference(
+                                                           adapterIndex,
+                                                           requestHighPerformanceAdapter == T100TRUE ? DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE : DXGI_GPU_PREFERENCE_UNSPECIFIED,
+                                                           IID_PPV_ARGS(&adapter)
+                                                           ));
+            ++adapterIndex
+            )
+        {
+            DXGI_ADAPTER_DESC1          desc;
+            adapter->GetDesc1(&desc);
+
+            if(desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+            {
+                continue;
+            }
+
+            if(SUCCEEDED(D3D12CreateDevice(
+                                           adapter.Get(),
+                                           D3D_FEATURE_LEVEL_12_0,
+                                           __uuidof(ID3D12Device),
+                                           T100NULL
+                                           )))
+            {
+                break;
+            }
+        }
+    }
+
+    if(adapter.Get() == T100NULL)
+    {
+        for(
+            UINT adapterIndex = 0;
+            SUCCEEDED(pFactory->EnumAdapters1(adapterIndex, &adapter));
+            ++adapterIndex
+            )
+        {
+            DXGI_ADAPTER_DESC1          desc;
+            adapter->GetDesc1(&desc);
+
+            if(desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
+            {
+                continue;
+            }
+
+            if(SUCCEEDED(D3D12CreateDevice(
+                                           adapter.Get(),
+                                           D3D_FEATURE_LEVEL_12_0,
+                                           __uuidof(ID3D12Device),
+                                           T100NULL
+                                           )))
+            {
+                break;
+            }
+        }
+    }
+
+    *ppAdapter = adapter.Detach();
+}
+
+T100VOID T100DXBase::CreateCommandQueue()
+{
+    D3D12_COMMAND_QUEUE_DESC            queueDesc           = {};
+
+    queueDesc.Flags         = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    queueDesc.Type          = D3D12_COMMAND_LIST_TYPE_DIRECT;
+
+    ThrowIfFailed(m_device->CreateCommandQueue(
+                                               &queueDesc,
+                                               IID_PPV_ARGS(&m_commandQueue)
+                                               ));
+}
+
+T100VOID T100DXBase::CreateSwapChain()
+{
+    DXGI_SWAP_CHAIN_DESC1               swapChainDesc       = {};
+
+    swapChainDesc.BufferCount           = m_frameCount;
+    swapChainDesc.Width                 = m_width;
+    swapChainDesc.Height                = m_height;
+    swapChainDesc.Format                = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferUsage           = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.SwapEffect            = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    swapChainDesc.SampleDesc.Count      = 1;
+
+    ComPtr<IDXGISwapChain1>             swapChain;
+
+    ThrowIfFailed(m_factory->CreateSwapChainForHwnd(
+                                                    m_commandQueue.Get(),
+                                                    m_hwnd,
+                                                    &swapChainDesc,
+                                                    T100NULL,
+                                                    T100NULL,
+                                                    &swapChain
+                                                    ));
+
+    ThrowIfFailed(m_factory->MakeWindowAssociation(
+                                                   m_hwnd,
+                                                   DXGI_MWA_NO_ALT_ENTER
+                                                   ));
+
+    ThrowIfFailed(swapChain.As(&m_swapChain));
+    m_frameIndex    = m_swapChain->GetCurrentBackBufferIndex();
+}
+
+T100VOID T100DXBase::CreateRtvHeap()
+{
+    D3D12_DESCRIPTOR_HEAP_DESC          rtvHeapDesc         = {};
+
+    rtvHeapDesc.NumDescriptors          = m_frameCount;
+    rtvHeapDesc.Type                    = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+    rtvHeapDesc.Flags                   = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+
+    ThrowIfFailed(m_device->CreateDescriptorHeap(
+                                                 &rtvHeapDesc,
+                                                 IID_PPV_ARGS(&m_rtvHeap)
+                                                 ));
+    m_rtvDescriptorSize     = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+}
+
+T100VOID T100DXBase::CreateDsvHeap()
+{
+    D3D12_DESCRIPTOR_HEAP_DESC          dsvHeapDesc         = {};
+
+    dsvHeapDesc.NumDescriptors          = 1;
+    dsvHeapDesc.Type                    = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+    dsvHeapDesc.Flags                   = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+
+    ThrowIfFailed(m_device->CreateDescriptorHeap(
+                                                 &dsvHeapDesc,
+                                                 IID_PPV_ARGS(&m_dsvHeap)
+                                                 ));
+}
+
+T100VOID T100DXBase::CreateCbvHeap()
+{
+    D3D12_DESCRIPTOR_HEAP_DESC          cbvSrvHeapDesc      = {};
+
+    cbvSrvHeapDesc.NumDescriptors       =
+        m_frameCount * m_cityRowCount * m_cityColumnCount
+        + 1;
+    cbvSrvHeapDesc.Type                 = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+    cbvSrvHeapDesc.Flags                = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+
+    ThrowIfFailed(m_device->CreateDescriptorHeap(
+                                                 &cbvSrvHeapDesc,
+                                                 IID_PPV_ARGS(&m_cbvSrvHeap)
+                                                 ));
+    m_cbvSrvDescriptorSize      = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+}
+
+T100VOID T100DXBase::CreateSamplerHeap()
+{
+    D3D12_DESCRIPTOR_HEAP_DESC          samplerHeapDesc     = {};
+
+    samplerHeapDesc.NumDescriptors      = 1;
+    samplerHeapDesc.Type                = D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER;
+    samplerHeapDesc.Flags               = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+
+    ThrowIfFailed(m_device->CreateDescriptorHeap(
+                                                 &samplerHeapDesc,
+                                                 IID_PPV_ARGS(&m_samplerHeap)
+                                                 ));
+
+}
+
+T100VOID T100DXBase::CreateCommandAllocator()
+{
+    ThrowIfFailed(m_device->CreateCommandAllocator(
+                                                   D3D12_COMMAND_LIST_TYPE_DIRECT,
+                                                   IID_PPV_ARGS(&m_commandAllocator)
+                                                   ));
 }
