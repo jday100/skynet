c9e0bff02d0e50b65b3054740a5b0791873ca7eb
jday100 <jdayterminator@outlook.com>
2025-01-12 17:21:27 +0800

    Modify

--- Projects/Game/Source/T1003DEditor/include/dx12/T100DX12Renderer.h
+++ Projects/Game/Source/T1003DEditor/include/dx12/T100DX12Renderer.h
@@ -1,11 +1,10 @@
 #ifndef T100DX12RENDERER_H
 #define T100DX12RENDERER_H
 
-#include "d3d12/DirectXMath.h"
 #include "T100DX12Base.h"
-#include "T100Entity.h"
-
-using namespace DirectX;
+#include "T100DX12Timer.h"
+#include "T100DX12Camera.h"
+#include "T100DX12Resource.h"
 
 class T100DX12Renderer : public T100DX12Base
 {
@@ -13,25 +12,103 @@ class T100DX12Renderer : public T100DX12Base
         T100DX12Renderer();
         virtual ~T100DX12Renderer();
 
-        T100VOID                            Append(T100Entity*);
+        virtual T100VOID                        Create(HWND, UINT, UINT);
+        virtual T100VOID                        Destroy();
+
+        virtual T100VOID                        Start();
+        virtual T100VOID                        Stop();
+
+        virtual T100VOID                        Init();
+        virtual T100VOID                        Update();
+        virtual T100VOID                        Render();
+
+        virtual T100VOID                        SetSize(UINT, UINT);
+
+        virtual T100VOID                        OnKeyDown(UINT8);
+        virtual T100VOID                        OnKeyUp(UINT8);
+
+        virtual T100VOID                        Append(T100Entity*);
 
     protected:
-        ComPtr<ID3D12RootSignature>         m_rootSignature;
-        CD3DX12_VIEWPORT                    m_viewport;
-        CD3DX12_RECT                        m_scissorRect;
+        HWND                                    m_hwnd;
 
-        ComPtr<ID3D12Resource>              m_vertexBuffer;
-        D3D12_VERTEX_BUFFER_VIEW            m_vertexBufferView;
+        UINT                                    m_width;
+        UINT                                    m_height;
 
-        T100DX12_ENTITY_VECTOR              m_entities;
+        float                                   m_aspectRatio;
+        T100BOOL                                m_useWarpDevice;
+
+        std::wstring                            GetAssetFullPath(LPCWSTR assetName);
+        T100VOID                                SetCustomWindowText(LPCWSTR text);
 
     private:
-        virtual T100VOID                    LoadAssets();
-        virtual T100VOID                    PopulateCommandList();
+        static const UINT                       m_frameCount            = 3;
+        UINT                                    m_CityRowCount          = 10;
+        UINT                                    m_CityColumnCount       = 3;
+        T100BOOL                                m_useBundles            = T100TRUE;
 
-        float                               m_aspectRatio;
-        std::wstring                        m_assetsPath;
-        std::wstring                        GetAssetFullPath(LPCWSTR assetName);
+        ComPtr<ID3D12Device>                    m_device;
+        ComPtr<IDXGISwapChain3>                 m_swapChain;
+        ComPtr<ID3D12CommandQueue>              m_commandQueue;
+        ComPtr<ID3D12CommandAllocator>          m_commandAllocator;
+
+        ComPtr<ID3D12Resource>                  m_renderTargets[m_frameCount];
+        ComPtr<ID3D12Resource>                  m_depthStencil;
+
+        ComPtr<ID3D12RootSignature>             m_rootSignature;
+        ComPtr<ID3D12DescriptorHeap>            m_rtvHeap;
+        ComPtr<ID3D12DescriptorHeap>            m_cbvSrvHeap;
+        ComPtr<ID3D12DescriptorHeap>            m_dsvHeap;
+        ComPtr<ID3D12DescriptorHeap>            m_samplerHeap;
+
+        ComPtr<ID3D12PipelineState>             m_pipelineState1;
+        ComPtr<ID3D12PipelineState>             m_pipelineState2;
+        ComPtr<ID3D12GraphicsCommandList>       m_commandList;
+
+        CD3DX12_VIEWPORT                        m_viewport;
+        CD3DX12_RECT                            m_scissorRect;
+
+        UINT                                    m_numIndices;
+        ComPtr<ID3D12Resource>                  m_vertexBuffer;
+        ComPtr<ID3D12Resource>                  m_indexBuffer;
+        ComPtr<ID3D12Resource>                  m_texture;
+        D3D12_VERTEX_BUFFER_VIEW                m_vertexBufferView;
+        D3D12_INDEX_BUFFER_VIEW                 m_indexBufferView;
+
+        UINT                                    m_frameIndex;
+        UINT                                    m_frameCounter;
+        HANDLE                                  m_fenceEvent;
+        ComPtr<ID3D12Fence>                     m_fence;
+        UINT64                                  m_fenceValue;
+
+        UINT                                    m_cbvSrvDescriptorSize;
+        UINT                                    m_rtvDescriptorSize;
+
+        std::vector<T100DX12Resource*>          m_frameResources;
+        T100DX12Resource*                       m_pCurrentFrameResource;
+        UINT                                    m_currentFrameResourceIndex;
+
+
+        std::wstring                            m_assetsPath;
+        std::wstring                            m_title;
+
+        T100DX12Timer                           m_timer;
+        T100DX12Camera                          m_camera;
+
+        T100VOID                                LoadPipeline();
+        T100VOID                                LoadResource();
+
+        T100VOID                                LoadFile(ComPtr<ID3D12Resource>&, ComPtr<ID3D12Resource>&, ComPtr<ID3D12Resource>&);
+
+        T100VOID                                CreateFrameResources();
+        T100VOID                                PopulateCommandList(T100DX12Resource* pFrameResource);
+
+        T100VOID                                GetHardwareAdapter(
+                                                                   _In_      IDXGIFactory1*      pFactory,
+                                                                   _Outptr_result_maybenull_     IDXGIAdapter1**     ppAdapter,
+                                                                   T100BOOL  requestHighPerformanceAdapter = T100FALSE
+                                                                   );
+
 };
 
 #endif // T100DX12RENDERER_H
