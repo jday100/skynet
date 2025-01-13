1b25db485c12cb26d73cac172220a17185c657ea
jday100 <jdayterminator@outlook.com>
2025-01-13 14:31:35 +0800

    Modify

---
+++ Projects/Game/Source/T1003DEditor/include/dx/T100DXTriangle.h
@@ -0,0 +1,17 @@
+#ifndef T100DXTRIANGLE_H
+#define T100DXTRIANGLE_H
+
+#include "T100DXBase.h"
+
+class T100DXTriangle : public T100DXBase
+{
+    public:
+        T100DXTriangle();
+        virtual ~T100DXTriangle();
+
+    protected:
+
+    private:
+};
+
+#endif // T100DXTRIANGLE_H
---
+++ Projects/Game/Source/T1003DEditor/src/dx/T100DXTriangle.cpp
@@ -0,0 +1,12 @@
+#include "T100DXTriangle.h"
+
+T100DXTriangle::T100DXTriangle()
+    :T100DXBase()
+{
+    //ctor
+}
+
+T100DXTriangle::~T100DXTriangle()
+{
+    //dtor
+}
