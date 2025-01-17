02066efcdc52148db1a8d5a0c3c24e58a445b175
jday100 <jdayterminator@outlook.com>
2025-01-17 17:06:30 +0800

    Modify

--- Platform/Projects/T100DX12ModelViewer/include/core/Cube.h
+++ Platform/Projects/T100DX12ModelViewer/include/core/Cube.h
@@ -2,15 +2,47 @@
 #define CUBE_H
 
 
-class Cube
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// Developed by Minigraph
+//
+// Author(s):  Jack Elliott
+//
+
+#pragma once
+
+#include "GpuBuffer.h"
+#include "GraphicsCore.h"
+
+namespace Graphics
 {
-    public:
-        Cube();
-        virtual ~Cube();
+    namespace Shapes
+    {
+        namespace Cube
+        {
+            struct CubeVertex
+            {
+                XMFLOAT3 position;
+                XMFLOAT2 texcoord;
+                XMFLOAT3 normal;
+                XMFLOAT3 tangent;
+                XMFLOAT3 bitangent;
+            };
 
-    protected:
+            extern ByteAddressBuffer g_CubeVerts;
+            extern uint32_t g_NumVerts;
+            extern uint32_t g_NumIndicies;
 
-    private:
-};
+            void InitializeCubeBuffers();
+            void DestroyCubeBuffers();
+        }
+    }
+}
 
 #endif // CUBE_H
