27e87f5f5c196658325a2e606374ddf69c39cb8a
jday100 <jdayterminator@outlook.com>
2025-01-17 15:15:39 +0800

    Modify

--- Platform/Projects/T100DX12ModelViewer/include/model/MeshConvert.h
+++ Platform/Projects/T100DX12ModelViewer/include/model/MeshConvert.h
@@ -2,15 +2,55 @@
 #define MESHCONVERT_H
 
 
-class MeshConvert
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// Developed by Minigraph
+//
+// Author:  James Stanard
+//
+
+#pragma once
+
+#include "glTF.h"
+#include "../Core/Math/BoundingSphere.h"
+#include "../Core/Math/BoundingBox.h"
+
+#include <cstdint>
+#include <string>
+
+namespace Renderer
 {
-    public:
-        MeshConvert();
-        virtual ~MeshConvert();
+    using namespace Math;
 
-    protected:
+    struct Primitive
+    {
+        BoundingSphere m_BoundsLS;  // local space bounds
+        BoundingSphere m_BoundsOS;  // object space bounds
+        AxisAlignedBox m_BBoxLS;       // local space AABB
+        AxisAlignedBox m_BBoxOS;       // object space AABB
+        Utility::ByteArray VB;
+        Utility::ByteArray IB;
+        Utility::ByteArray DepthVB;
+        uint32_t primCount;
+        union
+        {
+            uint32_t hash;
+            struct {
+                uint32_t psoFlags : 16;
+                uint32_t index32 : 1;
+                uint32_t materialIdx : 15;
+            };
+        };
+        uint16_t vertexStride;
+    };
+}
 
-    private:
-};
+void OptimizeMesh( Renderer::Primitive& outPrim, const glTF::Primitive& inPrim, const Math::Matrix4& localToObject );
 
 #endif // MESHCONVERT_H
--- Platform/Projects/T100DX12ModelViewer/include/model/SponzaRenderer.h
+++ Platform/Projects/T100DX12ModelViewer/include/model/SponzaRenderer.h
@@ -1,16 +1,54 @@
 #ifndef SPONZARENDERER_H
 #define SPONZARENDERER_H
 
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// Developed by Minigraph
+//
+// Author(s):  James Stanard
+//
 
-class SponzaRenderer
+#pragma once
+
+#include <d3d12.h>
+
+class GraphicsContext;
+class ShadowCamera;
+class ModelH3D;
+class ExpVar;
+
+namespace Math
 {
-    public:
-        SponzaRenderer();
-        virtual ~SponzaRenderer();
+    class Camera;
+    class Vector3;
+}
 
-    protected:
+namespace Sponza
+{
+    void Startup( Math::Camera& camera );
+    void Cleanup( void );
 
-    private:
-};
+    void RenderScene(
+        GraphicsContext& gfxContext,
+        const Math::Camera& camera,
+        const D3D12_VIEWPORT& viewport,
+        const D3D12_RECT& scissor,
+        bool skipDiffusePass = false,
+        bool skipShadowMap = false );
+
+    const ModelH3D& GetModel();
+
+    extern Math::Vector3 m_SunDirection;
+    extern ShadowCamera m_SunShadow;
+    extern ExpVar m_AmbientIntensity;
+    extern ExpVar m_SunLightIntensity;
+
+}
 
 #endif // SPONZARENDERER_H
