708c9bb059264ac2840adaff9a46bb9a1937fed5
jday100 <jdayterminator@outlook.com>
2025-04-29 20:55:56 +0800

    Modify

--- Master/Projects/T100Thread/include/thread/T100Thread.h
+++ Master/Projects/T100Thread/include/thread/T100Thread.h
@@ -12,16 +12,9 @@ class T100Thread
         template<typename _Callable, typename... _Args>
         T100Thread(_Callable&& __f, _Args&&... __args)
         {
-            /*
-            #ifdef GTHR_ACTIVE_PROXY
-                // Create a reference to pthread_create, not just the gthr weak symbol.
-                auto __depend = reinterpret_cast<void(*)()>(&pthread_create);
-            #else
-                auto __depend = nullptr;
-            #endif
-            */
             m_threadPtr = T100NEW std::thread(std::forward<_Callable>(__f),
                      std::forward<_Args>(__args)...);
+            m_running   = T100TRUE;
         }
         virtual ~T100Thread();
 
