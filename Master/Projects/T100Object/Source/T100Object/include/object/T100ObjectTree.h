0bcad684e953962afdd535186975db4c16902843
jday100 <jdayterminator@outlook.com>
2025-05-04 13:45:15 +0800

    Modify

---
+++ Master/Projects/T100Object/Source/T100Object/include/object/T100ObjectTree.h
@@ -0,0 +1,16 @@
+#ifndef T100OBJECTTREE_H
+#define T100OBJECTTREE_H
+
+
+class T100ObjectTree
+{
+    public:
+        T100ObjectTree();
+        virtual ~T100ObjectTree();
+
+    protected:
+
+    private:
+};
+
+#endif // T100OBJECTTREE_H
---
+++ Master/Projects/T100Object/Source/T100Object/src/object/T100ObjectTree.cpp
@@ -0,0 +1,11 @@
+#include "T100ObjectTree.h"
+
+T100ObjectTree::T100ObjectTree()
+{
+    //ctor
+}
+
+T100ObjectTree::~T100ObjectTree()
+{
+    //dtor
+}
