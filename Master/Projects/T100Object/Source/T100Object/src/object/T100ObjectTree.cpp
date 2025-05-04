1e823fe14d0e118ba987d7dd4fbfa52d59feb26b
jday100 <jdayterminator@outlook.com>
2025-05-04 14:03:32 +0800

    Modify

--- Master/Projects/T100Object/Source/T100Object/src/object/T100ObjectTree.cpp
+++ Master/Projects/T100Object/Source/T100Object/src/object/T100ObjectTree.cpp
@@ -1,6 +1,6 @@
 #include "T100ObjectTree.h"
 
-T100ObjectTree::T100ObjectTree()
+T100ObjectTree::T100ObjectTree(T100Object*)
 {
     //ctor
 }
@@ -8,4 +8,19 @@ T100ObjectTree::T100ObjectTree()
 T100ObjectTree::~T100ObjectTree()
 {
     //dtor
+}
+
+T100VOID T100ObjectTree::AddChild(T100WSTRING label, T100Object* object)
+{
+    m_children.append(label, object);
+}
+
+T100Object* T100ObjectTree::GetChildPtr(T100WSTRING label)
+{
+    return m_children[label];
+}
+
+T100Object* T100ObjectTree::GetParentPtr()
+{
+    return m_parentPtr;
 }
