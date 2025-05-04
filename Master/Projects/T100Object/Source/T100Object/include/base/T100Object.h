2dd1fe5900174f2c64fa074bac2f2f55eae4851a
jday100 <jdayterminator@outlook.com>
2025-05-04 13:23:17 +0800

    Modify

---
+++ Master/Projects/T100Object/Source/T100Object/include/base/T100Object.h
@@ -0,0 +1,16 @@
+#ifndef T100OBJECT_H
+#define T100OBJECT_H
+
+
+class T100Object
+{
+    public:
+        T100Object();
+        virtual ~T100Object();
+
+    protected:
+
+    private:
+};
+
+#endif // T100OBJECT_H
---
+++ Master/Projects/T100Object/Source/T100Object/include/common/T100DataTypes.h
@@ -0,0 +1,31 @@
+#ifndef T100DATATYPES_H
+#define T100DATATYPES_H
+
+//常量
+#define     T100NULL                nullptr
+#define     T100TRUE                true
+#define     T100FALSE               false
+
+//数据类型定义
+typedef     void                    T100VOID;
+typedef     bool                    T100BOOL;
+
+//无符号
+typedef     unsigned char           T100BYTE;
+typedef     unsigned short          T100HWORD;
+typedef     unsigned int            T100WORD;
+typedef     unsigned long long      T100DWORD;
+
+typedef     unsigned int            T100UINT;
+
+//有符号
+typedef     char                    T100INT8;
+typedef     short                   T100INT16;
+typedef     int                     T100INT32;
+typedef     long long               T100INT64;
+
+typedef     int                     T100INT;
+typedef     long long               T100LONG;
+typedef     float                   T100FLOAT;
+
+#endif // T100DATATYPES_H
