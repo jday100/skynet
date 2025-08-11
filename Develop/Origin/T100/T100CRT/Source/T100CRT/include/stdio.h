af0cdc124771f115c7a4337e497714ccaaac9b80
jday100 <jdayterminator@outlook.com>
2025-08-11 21:11:23 +0800

    Modify

--- Develop/Origin/T100/T100CRT/Source/T100CRT/include/stdio.h
+++ Develop/Origin/T100/T100CRT/Source/T100CRT/include/stdio.h
@@ -1,6 +1,8 @@
 #ifndef STDIO_H
 #define STDIO_H
 
+int strlen(const char*);
+
 int printf(const char*, ...);
 
 #endif // STDIO_H
--- Develop/Origin/T100/T100CRT/Source/T100CRT/src/stdio.c
+++ Develop/Origin/T100/T100CRT/Source/T100CRT/src/stdio.c
@@ -1,7 +1,33 @@
 #include "stdio.h"
 
+#include <windows.h>
+
+int strlen(const char* value)
+{
+    int result  = 0;
+
+    do{
+        if(value[result++] == '\0'){
+            break;
+        }
+    }while(true);
+    return result;
+}
+
 int printf(const char* value, ...)
 {
+    HANDLE          handle;
+    DWORD           written;
+    INT             length;
+    const char*     buffer;
+
+    buffer  = value;
+    length  = strlen(buffer);
+
+    handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    WriteFile(handle, buffer, length, &written, NULL);
+
     return 0;
 }
 
