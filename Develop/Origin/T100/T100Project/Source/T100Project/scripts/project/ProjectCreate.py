7441ae3a3648650300f473687edb4867ccc17383
jday100 <jdayterminator@outlook.com>
2025-09-02 18:29:53 +0800

    Modify

--- Develop/Origin/T100/T100Project/Source/T100Project/scripts/project/ProjectCreate.py
+++ Develop/Origin/T100/T100Project/Source/T100Project/scripts/project/ProjectCreate.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 
 from xml.dom import minidom
 
@@ -109,92 +110,17 @@ def create_project_make():
     print("Run create project make...")
 
     global g_project_path
+    global g_project_exec
     global g_project_makefile
 
     try:
-        cmd = "%s\\%s" % (g_project_path, g_project_makefile)
-        with open(cmd, 'w', encoding='UTF-8') as makefile:
-            makefile.write("""
-import os
-import sys
-import subprocess
+        source  = "%s\\scripts\\project\\ProjectBuild.py" % (g_project_exec)
+        target  = "%s\\%s" % (g_project_path, g_project_makefile)
 
-g_project_path      = ""
-g_compiler_path     = ""
+        cmd = "copy %s %s" % (source, target)
 
-g_project_build     = ""
-g_project_code      = ""
-g_project_include   = ""
-g_project_source    = ""
+        shutil.copy(source, target)
 
-def args():
-    print("Run args...")
-    count = len(sys.argv)
-
-    if(count != 8):
-        return
-
-    global g_project_path
-    global g_compiler_path
-
-    global g_project_build
-    global g_project_code
-    global g_project_include
-    global g_project_source
-
-    value               = sys.argv[1]
-    g_project_path      = sys.argv[2]
-    g_compiler_path     = sys.argv[3]
-
-    g_project_build     = sys.argv[4]
-    g_project_code      = sys.argv[5]
-    g_project_include   = sys.argv[6]
-    g_project_source    = sys.argv[7]
-
-    match value:
-        case 'build':
-            build()
-        case 'clean':
-            clean()
-        case 'rebuild':
-            rebuild()
-
-
-def build():
-    print("Run project build...")
-
-    global g_project_path
-    global g_compiler_path
-
-    global g_project_build
-    global g_project_code
-    global g_project_include
-    global g_project_source
-
-    bin     = "%s\\\\g++ -I%s\\\\%s\\\\%s" % (g_compiler_path, g_project_path, g_project_code, g_project_include)
-
-    line    = "%s\\\\%s\\\\%s\\\\" % (g_project_path, g_project_code, g_project_source)
-
-    cmd     = "%s %s%s -o %s\\\\%s\\\\%s" % (bin, line, "Project.cpp", g_project_path, g_project_build, "main.exe")
-
-    print(cmd)
-
-    subprocess.call(cmd)
-
-    print("Success")
-
-def clean():
-    print("Run project clean...")
-
-def rebuild():
-    print("Run project rebuild...")
-    clean()
-    build()
-
-if __name__ == "__main__":
-    print("Run project build...")
-    args()
-            """)
         print("Write OK")
     except Exception as err:
         print("Error:{err}".format(err=err))
